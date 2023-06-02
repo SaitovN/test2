98c9dc974fbf71b3af5857e939ec3725bb72e33a
SaitovN <nuriksaitov03@gmailcom>
2023-06-02 17:02:07 +0600

    Create test2.java
    test2

---
+++ test2.java
@@ -0,0 +1,15 @@
+package academy.devonline.java.basic.section01_setup.section05_setup;
+
+import java.util.Scanner;
+
+public class test2 {
+    public static void main(String[] args) {
+      String result = new Scanner(System.in).nextLine().repeat(3);
+      var a = 10;
+        System.out.println(result);
+//      for (int u = 0; u < a; u++){
+//          result= result+ u;
+//          System.out.println(u);
+//      }
+    }
+}
